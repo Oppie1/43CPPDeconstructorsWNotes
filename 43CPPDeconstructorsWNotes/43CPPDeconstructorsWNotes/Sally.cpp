6a38444cab5a0ea495951ec03b1266618218bc20
Oppie1 <atindall729@gmail.com>
2025-09-30 14:03:13 -0500

    Update Sally.cpp

--- 43CPPDeconstructorsWNotes/43CPPDeconstructorsWNotes/Sally.cpp
+++ 43CPPDeconstructorsWNotes/43CPPDeconstructorsWNotes/Sally.cpp
@@ -7,14 +7,16 @@ Sally::Sally() {
 	cout << "I am the consructor" << endl;
 }
 
-//A deconstuctor is code that runs automatically upon distruction of you object.  At end of program
-//when memory is getting freed, program is ending and objects are being destroyed this runs.
-//As soon as object is destroyed this runs.
 
 Sally::~Sally() {
 
 	cout << "I am the deconstructor" << endl;
 }
 
+//A deconstuctor is code that runs automatically upon distruction of you object.  At end of program
+//when memory is getting freed, program is ending and objects are being destroyed this runs.
+//As soon as object is destroyed this runs.
+
 //Whenever you are creating a deconstuctor can not carry parameters. It also has no return.  Not even void.
+
 //There is no deconstructor overloading.  You have 1 deconstuctor and that is it.
